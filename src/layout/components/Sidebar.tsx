e829c0ed3db054279d58c4d985077a1d0e9f6c5e
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-09-09 15:46:14 +0500

    dojdod

--- src/layout/components/Sidebar.tsx
+++ src/layout/components/Sidebar.tsx
@@ -1,14 +1,14 @@
-import { memo } from 'react';
-import { NavLink } from 'react-router-dom';
+import { memo } from "react";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="w-[250px] h-screen bg-slate-900 text-white fixed top-0 left-0">
       <div className="flex flex-col py-[30px] px-[10px] ">
-        <h2 className='text-[24px] pl-[4px] mb-[20px]'>Dashboard</h2>
+        <h2 className="text-[24px] pl-[4px] mb-[20px]">Dashboard</h2>
         <NavLink
           className={
-            "px-[12px] py-[12px] bg-[#36363666] rounded-[6px]  mb-[7px] duration-150 hover:bg-[#49494971] hover:pl-[16px]"
+            "navlink px-[12px] py-[12px] bg-[#36363666] rounded-[6px]  mb-[7px] duration-150 hover:bg-[#49494971] hover:pl-[16px]"
           }
           to={"/"}
         >
@@ -16,7 +16,7 @@ const Sidebar = () => {
         </NavLink>
         <NavLink
           className={
-            "px-[12px] py-[12px] bg-[#36363666] rounded-[6px] mb-[7px] duration-150 hover:bg-[#49494971] hover:pl-[16px]"
+            "navlink px-[12px] py-[12px] bg-[#36363666] rounded-[6px] mb-[7px] duration-150 hover:bg-[#49494971] hover:pl-[16px]"
           }
           to={"/product"}
         >
@@ -24,7 +24,7 @@ const Sidebar = () => {
         </NavLink>
         <NavLink
           className={
-            "px-[12px] py-[12px] bg-[#36363666] rounded-[6px] mb-[7px] duration-150 hover:bg-[#49494971] hover:pl-[16px]"
+            "navlink px-[12px] py-[12px] bg-[#36363666] rounded-[6px] mb-[7px] duration-150 hover:bg-[#49494971] hover:pl-[16px]"
           }
           to={"/user"}
         >
