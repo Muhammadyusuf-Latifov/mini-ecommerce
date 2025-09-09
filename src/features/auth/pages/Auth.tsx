8f063d2ce00bd7d91e460bf94a71b38eb9f306b0
Muhammadyusuf-Latifov <longhaochen200@gmail.com>
2025-09-09 16:33:03 +0500

    djiodj

--- src/features/auth/pages/Auth.tsx
+++ src/features/auth/pages/Auth.tsx
@@ -1,11 +1,10 @@
 import { memo } from "react";
-// import { useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-// import type { RootState } from "../../../app/store";
+import type { RootState } from "../../../app/store";
 
 const Auth = () => {
-  const token = true;
-  // useSelector((state: RootState) => state.auth.token);
+  const token = useSelector((state: RootState) => state.auth.token);
 
   return token ? <Outlet /> : <Navigate replace to={"/login"} />;
 };
